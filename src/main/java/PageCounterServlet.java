0634517da9aba30a41a2dc836908516612c4ea10
Casey Friday <casey@friday-next.com>
2020-04-21 14:30:19 -0500

    Servlets Exercises

--- src/main/java/PageCounterServlet.java
+++ src/main/java/PageCounterServlet.java
@@ -5,13 +5,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name="HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name="PageCounterServlet", urlPatterns = "/page-count")
+public class PageCounterServlet extends HttpServlet {
+
+    public static int count;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<h1>Hello, World!</h1>");
+    }
+
+    public void incrementCount() {
+        count++;
     }
 }
